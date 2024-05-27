59f3f602c81fcbc13492e404a5681f5f939327b9
Katy248 <petrovanton247@gmail.com>
2024-05-27 21:10:32 +0300

    Add vulkan packages

--- install-nvidia.sh
+++ install-nvidia.sh
@@ -3,7 +3,9 @@ echo "Remove \`kms\` from HOOKS in file /etc/mkinitcpio.conf and then rerun scri
 echo "Othervise press Enter"
 read
 
-sudo pacman -Syu nvidia-dkms nvidia-utils nvidia-settings lib32-nvidia-utils opencl-nvidia --no-confirm
+sudo pacman -Syu --no-confirm\
+  nvidia-dkms nvidia-utils nvidia-settings lib32-nvidia-utils opencl-nvidia vulkan-icd-loader lib32-vulkan-icd-loader
+
 sudo mkinitcpio -P
 
 
