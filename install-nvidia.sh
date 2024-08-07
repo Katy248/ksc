89b7230ff1793a0b0de154268000df35df8db633
Katy248 <petrovanton247@gmail.com>
2024-08-07 09:03:12 +0300

    Разрешил конфликты

--- install-nvidia.sh
+++ install-nvidia.sh
@@ -1,12 +1,26 @@
 
+bold=$(tput bold)
+normal=$(tput sgr0)
+
 echo "Remove \`kms\` from HOOKS in file /etc/mkinitcpio.conf and then rerun script"
 echo "Othervise press Enter"
 read
 
-sudo pacman -Syu --no-confirm\
-  nvidia-dkms nvidia-utils nvidia-settings lib32-nvidia-utils opencl-nvidia vulkan-icd-loader lib32-vulkan-icd-loader
+echo "There is several drivers for nvidia:"
+echo " - ${bold}nvidia${normal} - for ${bold}linux${normal} kernel"
+echo " - ${bold}nvidia-lts${normal} - for ${boldl}inux-lts${normal} kernel"
+echo " - ${bold}nvidia-dkms${normal} - for other kernels"
+echo ""
 
+echo "Input your driver:"
+printf "Driver: "
+read -r driver
+
+printf "This script will use sudo, continue? [Enter/Ctrl+C] "
+read
+
+sudo pacman -Syu "${driver}" nvidia-utils nvidia-settings lib32-nvidia-utils opencl-nvidia vulkan-icd-loader lib32-vulkan-icd-loader --noconfirm
 sudo mkinitcpio -P
 
-
+echo -e "\n\n"
 echo "Don't forget to reboot after running this script"
