d7a44f7136c31caf24373813dacc3923d14d7ada
Katy248 <petrovanton247@gmail.com>
2024-09-15 20:28:41 +0300

    Fix dnf naming

--- clear-packages.sh
+++ clear-packages.sh
@@ -4,15 +4,15 @@ execute() {
   if command -v nix-env ; then
     home-manager expire-generations -2days
     nix-env --delete-generations 2d
-    nix-store --gc 
+    nix-store --gc
   fi
 
-  if command -v flatpak ; then 
+  if command -v flatpak ; then
     flatpak uninstall --unused -y
   fi
 
-  if command -v dnf ; then 
-    sudo dnfo --verbose clean dbcache all
+  if command -v dnf ; then
+    sudo dnf --verbose clean dbcache all
   fi
 }
 
