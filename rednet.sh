35dac5f6e7da7a68012b91a8c7b01d1ef83d2830
Katy248 <petrovanton247@gmail.com>
2024-09-23 16:40:39 +0300

    Add adapters list command

--- rednet.sh
+++ rednet.sh
@@ -3,17 +3,32 @@
 print_help() {
   echo "Usage:"
   echo "    rednet.sh <adapter-name>"
+  echo "    rednet.sh adapters"
+  echo "    rednet.sh help" 
   exit 0
 }
+
+print_adapters() {
+  nmcli --fields DEVICE --terse connection show --active
+}
+
+
 
 
 if [[ $# -lt 1 ]]; then
   echo "Network adapter not specified" >&2
+  echo "You can use following adapters:"
+  print_adapters
   exit 1
 fi
 
 if [[ $1 == "help" ]]; then 
   print_help
+fi
+
+if [[ $1 == "adapters" ]]; then 
+  print_adapters
+  exit 0
 fi
 
 adapter=$1
