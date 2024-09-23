17f525fc911a9d8bd10b23a3d9100d06bc0826e4
Katy248 <petrovanton247@gmail.com>
2024-09-23 16:16:05 +0300

    Added some cli parsing logic

--- rednet.sh
+++ rednet.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+print_help() {
+  echo "Usage rednet.sh <adapter-name>"
+  exit 0
+}
+
+
+if [[ $# -lt 1 ]]; then
+  echo "Network adapter not specified" >&2
+  exit 1
+fi
+
+if [[ $1 == "help" ]]; then 
+  print_help
+fi
+
 adapter=$1
 
 echo "Network settings for ${adapter}:"
