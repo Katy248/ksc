f02dc4dfd5f1e95cde2b31455dff8c8a395e99b8
Katy248 <petrovanton247@gmail.com>
2024-09-23 10:22:11 +0300

    Updated scripts

--- clear-packages.sh
+++ clear-packages.sh
@@ -17,8 +17,9 @@ execute() {
 
   if command -v docker ; then
     sudo docker container prune -f
-    sudo docker image prune -f
-    sudo docker volume prune -f
+    sudo docker image     prune -f
+    sudo docker volume    prune -f
+    sudo docker buildx    prune -f
   fi
 }
 
