8674f86cd7aca280ec0e1a1b3cf3d33b511441c8
Katy248 <petrovanton247@gmail.com>
2024-10-09 16:05:54 +0300

    Updated keybindings

--- gnome-alt-shift.sh
+++ gnome-alt-shift.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Shift>Alt_L', '<Shift>Alt_R']"
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Super>Space', '<Shift>Alt_L', '<Shift>Alt_R']"
 gsettings get org.gnome.desktop.wm.keybindings switch-input-source
 
 gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "['<Alt>Shift_L', '<Alt>Shift_R']"
