f012161f8cac67ed1510d75e062b2a0ab0322a42
Katy248 <petrovanton247@gmail.com>
2025-01-28 16:06:59 +0300

    Add snap

--- update-all.sh
+++ update-all.sh
@@ -13,21 +13,36 @@ program_exist() {
   return 1
 }
 
+print_sudo_warn() {
+  gum log \
+    --level warn \
+    --level.foreground "#fabd2f" \
+    "Sudo will be used"
+}
+
 execute() {
   if program_exist pacman; then
+    print_sudo_warn
     sudo pacman -Syu --noconfirm
   fi
 
   if program_exist dnf; then
+    print_sudo_warn
     sudo dnf upgrade -y
   fi
 
   if program_exist nala; then
+    print_sudo_warn
     sudo nala update
     sudo nala upgrade -y
   elif program_exist apt; then
+    print_sudo_warn
     sudo apt update
     sudo apt upgrade -y
+  fi
+
+  if program_exist snap; then
+    snap refresh
   fi
 
   if program_exist flatpak; then
